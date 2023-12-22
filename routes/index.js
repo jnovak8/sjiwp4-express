a4c247920ebe04f33ec7dbfb6f13626a8626db98
Jan N <jannovak.work@gmail.com>
2023-12-22 10:10:14 +0100

    Login Hashing Working

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { checkAuthCookie } = require("../services/auth.js");
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+// GET /
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+router.get("/protected", checkAuthCookie, function (req, res, next) {
+
+  if (req.user) {
+    console.log("USER SIGNED IN");
+  } else {
+    console.log("NO USER");
+  }
+
+  res.send("DONE");
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,89 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+const { db } = require("../services/db.js");
+const { getUserJwt } = require("../services/auth.js");
+const bcrypt = require("bcrypt");
 
-router.get('/signin', function(req, res, next) {
-  res.render('users/signin');
+// GET /users/signin
+router.get("/signin", function (req, res, next) {
+  res.render("users/signin", { result: { display_form: true } });
+});
+
+// SCHEMA signin
+const schema_signin = Joi.object({
+  email: Joi.string().email().max(50).required(),
+  password: Joi.string().min(3).max(50).required()
+});
+
+// POST /users/signin
+router.post("/signin", function (req, res, next) {
+  // do validation
+  const result = schema_signin.validate(req.body);
+  if (result.error) {
+    res.render("users/signin", { result: { validation_error: true, display_form: true } });
+    return;
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const stmt = db.prepare("SELECT * FROM users WHERE email = ?");
+  const dbResult = stmt.get(email);
+
+  if (dbResult){
+    const passwordHash = dbResult.password;
+    const compareResult = bcrypt.compareSync(password, passwordHash);
+
+    if (!compareResult){
+      res.render("users/signin", { result: { invalid_credentials: true } });
+    }
+  }
+
+  if (dbResult) {
+    const token = getUserJwt(dbResult.id, dbResult.email, dbResult.name, dbResult.role);
+    res.cookie("auth", token);
+
+    res.render("users/signin", { result: { success: true } });
+  } else {
+    res.render("users/signin", { result: { invalid_credentials: true } });
+  }
+});
+
+// SCHEMA signin
+const schema_signup = Joi.object({
+  name: Joi.string().min(3).max(50).required(),
+  email: Joi.string().email().max(50).required(),
+  password: Joi.string().min(3).max(50).required(),
+  password_check: Joi.ref("password")
+});
+
+// GET /users/signup
+router.get("/signup", function (req, res, next) {
+  res.render("users/signup", { result: { display_form: true } });
+});
+
+// POST /users/signup
+router.post("/signup", function (req, res, next) {
+  // do validation
+  const result = schema_signup.validate(req.body);
+  if (result.error) {
+    res.render("users/signup", { result: { validation_error: true, display_form: true } });
+    return;
+  }
+
+  const passwordHash = bcrypt.hashSync(req.body.password, 10);
+
+  const stmt = db.prepare("INSERT INTO users (email, password, name, signed_at, role) VALUES (?, ?, ?, ?, ?);");
+
+  const insert_result = stmt.run(req.body.email, passwordHash, req.body.name, Date.now(), "user"); 
+
+  if (insert_result.changes && insert_result.changes==1){
+    res.render("users/signup", { result: { success: true } });
+  }else{
+    res.render("users/signup", { result: { database_error: true } });
+  }
+
 });
 
 module.exports = router;
