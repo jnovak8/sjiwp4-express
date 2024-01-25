ad3ac127b959ecc852bc756580f73ca9ec66b58f
Jan N <jannovak.work@gmail.com>
2024-01-25 10:59:12 +0100

    User Data Change

--- services/auth.js
+++ services/auth.js
@@ -1,6 +1,7 @@
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const jwt = require("jsonwebtoken");
+const { db } = require("./db.js");
 
 function getUserJwt(id, email, name, role, expDays = 7) {
     const tokenData = {
@@ -22,12 +23,11 @@ function getUserJwt(id, email, name, role, expDays = 7) {
 
 // MIDDLEWARE FOR AUTHENTICATION CHECK
 function authRequired(req, res, next) {
-    if(!req.user) throw new Error("Potrebna je prijava u sustav");
+    if (!req.user) throw new Error("Potrebna je prijava u sustav");
     next();
 }
-
 // MIDDLEWARE FOR PARSING AUTH COOKIE
-function parseAuthCookie(req, res, next){
+function parseAuthCookie(req, res, next) {
     const token = req.cookies[process.env.AUTH_COOKIE_NAME];
     let result = null;
     try {
@@ -41,8 +41,20 @@ function parseAuthCookie(req, res, next){
     next();
 }
 
+function checkEmailUnique(email){
+    const stmt = db.prepare("SELECT count(*) FROM users WHERE email = ?;");
+    const result = stmt.get(email);
+
+    if (result["count(*)"] >= 1){
+        return false;
+    } else {
+        return true;
+    }
+}
+
 module.exports = {
     getUserJwt,
     authRequired,
-    parseAuthCookie
+    parseAuthCookie,
+    checkEmailUnique
 };
